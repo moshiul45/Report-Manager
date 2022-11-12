5e5514b36be2a14701aaff83ec87bb9b138377fe
Md. Moshiul Azam <azammoshiul45@gmail.com>
2022-11-12 23:29:42 +0600

    User APi created to check data insertion

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const User = require("../models/user/user");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async (req, res, next) => {
+  try {
+    const { body: user_body } = req;
+    const newUser = new User(user_body);
+    const user_res = await newUser.save();
+    if (user_res) {
+      return res.status(200).json({
+        status: true,
+        message: "Test is successful. User data inserted succesfully",
+      });
+    }
+  } catch (error) {
+    return res.status(404).json({
+      status: false,
+      message: error?.message || "Server error!!!",
+    });
+  }
 });
 
 module.exports = router;
