ba5537059b2704a11f23c269403256eaeb024550
Md. Moshiul Azam <azammoshiul45@gmail.com>
2022-11-13 23:44:29 +0600

    update user

--- controllers/user/user.js
+++ controllers/user/user.js
@@ -1,7 +1,25 @@
 const User = require("../../models/user/user");
 const { successResponse } = require("../../utils/response");
+
 //admin can add employee/user for the organization through this controller
 exports.add_user = async (req, res) => {
+  try {
+    const { body: user_body } = req;
+    const newUser = new User(user_body);
+    const user_res = await newUser.save();
+    if (user_res) {
+      successResponse(res, 200, true, "New User Added Successfully!");
+    }
+  } catch (error) {
+    return res.status(404).json({
+      status: false,
+      message: error?.message || "Server error!!!",
+    });
+  }
+};
+
+//admin can update employee/user for the organization through this controller
+exports.update_user = async (req, res) => {
   try {
     const { body: user_body } = req;
     const newUser = new User(user_body);
